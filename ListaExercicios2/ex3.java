74ceea0841ea5bfc3920bbab53d18f442dcb304b
GabrielSouzaGit <gbl7889@gmail.com>
2021-03-29 20:57:24 -0300

    Create ex3.java

---
+++ ListaExercicios2/ex3.java
@@ -0,0 +1,17 @@
+'''Altere o programa anterior de forma que ele
+diga se o número informado pelo usuário está
+entre 10 e 20 ou não, porém sem utilizar o
+operador lógico E (&&)'''
+
+import java.util.Scanner;
+public class ex3{
+    public static void main(String[] args){
+    
+    Scanner leitura = new Scanner(System.in);
+    int num;
+
+    System.out.print("Informe um numero: ");
+    num = Integer.parseInt(leitura.nextLine());
+    System.out.println(num < 10 || num > 20 ? "Numero nao esta entre 10 e 20!!" : "Numero esta entre 10 e 20!!");
+    }
+}
